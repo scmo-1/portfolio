9e4768de06cff9c85506269a19a5f4e633858bbc
Simon Olsson <scmolsson@pm.me>
2025-09-14 15:14:58 +0200

    add lenis scroll

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,9 @@
+"use client";
 import { Oswald, Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/Header";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 const OswaldFont = Oswald({ subsets: ["latin"], variable: "--font-oswald" });
 const BricolageFont = Bricolage_Grotesque({
@@ -9,6 +12,16 @@ const BricolageFont = Bricolage_Grotesque({
 });
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.4,
+    });
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <html
       lang="en"
