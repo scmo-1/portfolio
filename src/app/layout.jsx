299223102cee1962e0c19023a4eb77eec50dae82
Simon Olsson <scmolsson@pm.me>
2025-09-21 18:44:23 +0200

    add metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,42 @@ import { useEffect, useRef } from "react";
 import Lenis from "lenis";
 import { usePathname } from "next/navigation";
 
+export const metadata = {
+  title: "Web developer portfolio",
+  description: "Portfolio for Simon Olsson - web developer",
+  keywords: [
+    "web",
+    "developer",
+    "frontend",
+    "fullstack",
+    "React",
+    "Next.js",
+    "portfolio",
+    "JavaScript",
+    "CSS",
+    "HTML",
+  ],
+
+  authors: [{ name: "Simon Olsson", url: "https://scmo.dev" }],
+  creator: "Simon Olsson",
+  publisher: "Simon Olsson",
+  applicationName: "Simon Olsson Portfolio",
+  category: "Portfolio / Web Development",
+  language: "en-US",
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+
+  metadataBase: new URL("https://scmo.dev"),
+};
+
 const OswaldFont = Oswald({ subsets: ["latin"], variable: "--font-oswald" });
 const BricolageFont = Bricolage_Grotesque({
   subsets: ["latin"],
